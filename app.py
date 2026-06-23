a1a73e33bed3e8257adb6fa5706d04a0ed64be03
zawlinmyinta1-del <zawlinmyint.a1@gmail.com>
2026-06-23 17:36:18 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 import streamlit as st
 import sys
 import io
-from mapping import translate_code, burmese_to_python
+from mapping import translate_code
 
 st.title("MyanmarCode (Python Converter)")
 
-# စာရိုက်ရန်နေရာ
 user_input = st.text_area("ဒီမှာ မြန်မာလို Code ရိုက်ပါ", height=150)
 
 if st.button("Run"):
     try:
         # ဘာသာပြန်ခြင်း
         python_code = translate_code(user_input)
-        
-        # ပြောင်းသွားတဲ့ Code ကို ပြခြင်း
         st.write("ပြောင်းလဲလိုက်သော Python Code:")
         st.code(python_code, language='python')
         
         # ရလဒ်ကို အမှန်တကယ် Run ခြင်း
         st.write("ရလဒ်:")
+        # Output ကို ဖမ်းယူရန်
         old_stdout = sys.stdout
         sys.stdout = mystdout = io.StringIO()
         
-        # Python Code အဖြစ် Run ခြင်း
         exec(python_code)
         
         sys.stdout = old_stdout
         st.success(mystdout.getvalue())
-        
     except Exception as e:
         st.error(f"အမှားအယွင်းရှိသည်: {e}")
