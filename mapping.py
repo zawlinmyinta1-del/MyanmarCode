558d09e9987d4b693e01a72d2c46def9f9d1023f
zawlinmyinta1-del <zawlinmyint.a1@gmail.com>
2026-06-24 04:16:24 +0630

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,21 +1,6 @@
-def translate_code(myanmar_code):
-    # မြန်မာဂဏန်းများကို အင်္ဂလိပ်ဂဏန်းအဖြစ် ပြောင်းလဲခြင်း
-    num_map = {'၀': '0', '၁': '1', '၂': '2', '၃': '3', '၄': '4', 
-               '၅': '5', '၆': '6', '၇': '7', '၈': '8', '၉': '9'}
-    
-    translated = myanmar_code
-    for burmese, english in num_map.items():
-        translated = translated.replace(burmese, english)
-        
-    # စကားလုံးများကို Mapping ပြုလုပ်ခြင်း
-    mapping = {
-        "ထုတ် :": "print(",
-        "ဖြစ်သည်": "=",
-        "အကယ်၍": "if",
-        "မဟုတ်လျှင်": "else:" 
-    }
-    
-    for burmese, python in mapping.items():
-        translated = translated.replace(burmese, python)
-        
-    return translated
+mapping = {
+    "ထုတ် :": "print(",
+    "ဖြစ်သည်": "=",
+    "အကယ်၍": "if",
+    "မဟုတ်လျှင်": "else"
+}
