8b616665de49e146aed15e3f28ee9ee19661032d
zawlinmyinta1-del <zawlinmyint.a1@gmail.com>
2026-06-23 22:25:59 +0630

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,22 +1,24 @@
 def translate_code(myanmar_code):
-    # ၁။ ဂဏန်းပြောင်းခြင်း
+    # ၁။ မြန်မာဂဏန်းတွေကို English ဂဏန်းအဖြစ် ပြောင်းရန် Mapping
     num_map = {'၀': '0', '၁': '1', '၂': '2', '၃': '3', '၄': '4', 
                '၅': '5', '၆': '6', '၇': '7', '၈': '8', '၉': '9'}
+    
     translated = myanmar_code
-    for b, e in num_map.items():
-        translated = translated.replace(b, e)
+    for burmese, english in num_map.items():
+        translated = translated.replace(burmese, english)
         
-    # ၂။ Keyword Mapping
+    # ၂။ စကားလုံး Mapping
     mapping = {
         "ထုတ် :": "print(",
         "ဖြစ်သည်": "=",
         "အကယ်၍": "if",
-        "မဟုတ်လျှင်": "else:"
+        "မဟုတ်လျှင်": "else:" 
     }
-    for b, p in mapping.items():
-        translated = translated.replace(b, p)
+    
+    for burmese, python in mapping.items():
+        translated = translated.replace(burmese, python)
         
-    # ၃။ အကယ်၍ (if) စာကြောင်းအတွက် : ဖြည့်ပေးခြင်း
+    # ၃။ အကယ်၍ (if) နောက်မှာ : မပါရင် ဖြည့်ပေးခြင်း
     if "if" in translated and not translated.strip().endswith(":"):
         translated = translated + ":"
         
