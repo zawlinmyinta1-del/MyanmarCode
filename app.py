ba823f5393c3851feaf50ea45a5b6a23a0226ec6
zawlinmyinta1-del <zawlinmyint.a1@gmail.com>
2026-06-24 04:29:45 +0630

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,16 @@ if 'my_globals' not in st.session_state:
     st.session_state.my_globals = {}
 
 st.title("MyanmarCode Compiler")
-
 code_input = st.text_area("ဒီမှာ Code ရိုက်ပါ", height=200)
 
 if st.button("Run"):
     try:
-        # ဘာသာပြန်ခြင်း
         translated_code = translate_code(code_input)
-        
-        # Output ဖမ်းယူရန်
         old_stdout = sys.stdout
         sys.stdout = mystdout = io.StringIO()
-        
-        # Run လုပ်ခြင်း
         exec(translated_code, st.session_state.my_globals)
-        
         sys.stdout = old_stdout
         st.success("ရလဒ်:")
         st.write(mystdout.getvalue())
-        
     except Exception as e:
         st.error(f"Error တက်နေသည်: {e}")
