e0b9eac4a15cbbca4974df5eac14ad908b561680
zawlinmyinta1-del <zawlinmyint.a1@gmail.com>
2026-06-24 08:40:59 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
 import streamlit as st
 from mapping import translate_code
 
-st.set_page_config(page_title="MyanmarCode Compiler", page_icon="💻")
-st.title("💻 MyanmarCode Compiler")
+st.title("MyanmarCode Compiler")
 
-# Variable တွေကို သိမ်းထားမယ့်နေရာ
-if 'variables' not in st.session_state:
-    st.session_state.variables = {}
+# Variable များ သိမ်းဆည်းရန်
+if 'vars' not in st.session_state:
+    st.session_state.vars = {}
 
-code_input = st.text_area("ဒီမှာ Code ရိုက်ပါ", height=150)
-
+code = st.text_area("Code ရိုက်ပါ")
 if st.button("Run"):
     try:
-        translated = translate_code(code_input)
-        exec(translated, {}, st.session_state.variables)
-        
-        # ရလဒ်ကို အလှဆင်ပြီးပြသခြင်း
-        st.success("✅ အောင်မြင်စွာ ဘာသာပြန်ပြီးပါပြီ")
-        
-        # Dictionary ထဲက တန်ဖိုးတွေကို အလှဆင်ပြသခြင်း
-        if st.session_state.variables:
-            st.info("မှတ်သားထားသော Variable များ:")
-            for key, value in st.session_state.variables.items():
-                st.write(f"🔹 **{key}** : `{value}`")
-        
+        py_code = translate_code(code)
+        exec(py_code, {}, st.session_state.vars)
+        st.success("အောင်မြင်သည်!")
+        st.write(st.session_state.vars)
     except Exception as e:
-        st.error(f"⚠️ အမှား - {e}")
+        st.error(f"အမှား - {e}")
